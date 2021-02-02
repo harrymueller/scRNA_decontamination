96015ece8c70e614f29587f89b01427c46467c59
harrymueller <harry@mueller.com.au>
2021-02-02 11:51:36 +0800

    Working on refactoring scripts

--- main.R
+++ main.R
@@ -1,17 +1,42 @@
-#!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
 # loading general functions
 source("../improved_scripts/scripts/general_functions.R")
 source("../improved_scripts/scripts/decontamination_functions.R")
+source("../improved_scripts/scripts/analysis_functions.R")
 
-load_libraries()
-
-# getting config
+if (!requireNamespace('config', quietly=T))
+  stop("R Package 'config' not installed.")
+# DO NOT LOAD CONFIG - CLASHES W/ SEURAT MERGE
 config <- get_config(args)
+if (F) {
+#load_libraries()
+required_libs = c(
+  c("Seurat", "readxl", "varhandle", "MASS", "dplyr", "tidyverse"),
+  if ("decontaminate" %in% config$process) c("SoupX", "celda") else NULL,
+  if ("analyse" %in% config$process) c("ggplot2", "xlsx", "reshape2","plotly","cowplot","patchwork") else NULL
+)
+uninstalled = c()
 
+for (i in required_libs) {
+  if (!requireNamespace(i, quietly=T)) {
+    # lib not found
+    uninstalled = append(uninstalled, i)
+  } else {
+    # loading lib
+    suppressMessages({suppressWarnings({
+      library(i, character.only=T)
+    })})
+  }
+}
 
-
+if (!is.null(uninstalled)) {
+  stop(paste("The following packages are required but not installed:",paste(uninstalled, collapse=", ")))
+} else {
+  log_print("All libraries loaded successfully")
+}
+}
+load_libraries()
 
 decontaminate_samples <- function (config, files, current_method) {
   samples = as.list(config$sample_ids)
@@ -23,22 +48,93 @@ decontaminate_samples <- function (config, files, current_method) {
     sample_id = config$sample_ids[i]
     
     print(paste("Starting",sample_id))
-    samples[[sample_id]] = get_sample(sample_id, files$dir[i], current_method, files$CellAnnotations, files$special, config$recluster)
-    
+    samples[[sample_id]] = get_sample(sample_id, files$dir[i], current_method, files$CellAnnotations, files$special[i], !(config$recluster == F || (current_method == "none" || current_method == "no_decontamination")))
+                                      # ^ Dont use new clusters if current method is no decontamination
     # ensuring formatting of cell barcodes is the same (across all analyses)
-    samples[[sample_id]]$seurat.decont = fix_barcodes(samples[[sample_id]]$seurat.decont, paste(output, "errors.txt", sep="/"))
+    samples[[sample_id]]$seurat = fix_barcodes(samples[[sample_id]]$seurat)
   }
   
-  save_matrices(samples, paste(files$output, "/matrices/",sep=""))
-  save_Rda(samples, paste(files$output, "/Rda/decontaminated_samples/", ))
+  #save_matrices(samples, paste(files$output, "/matrices/",sep=""))
+  
+  ################################################################################################
+  ### Combining seurat objects
+  ################################################################################################
+  print("Combining seurat objects")
+  
+  # loops through each object in samples <- adds metadata to `seurat.decont` 
+  samples.seurat <- lapply(samples, function(x) {
+    x$seurat@meta.data$orig.ident = x$sample_id
+    x$seurat@meta.data$preservation = if (length(str_split(x$sample_id, "_",simplify=TRUE))>2) "MeOH" else "fresh"
+    x$seurat@meta.data$method = "decont"
+    return(x$seurat)
+  })
+  
+
+  # combining
+  samples.combined <- merge(samples.seurat[[1]], samples.seurat[2:6], add.cell.ids = config$sample_ids)
+
+  # factoring metadata
+  samples.combined@meta.data$orig.ident = factor(samples.combined@meta.data$orig.ident)
+  samples.combined@meta.data$preservation = factor(samples.combined@meta.data$preservation)
+  samples.combined@meta.data$method = factor(samples.combined@meta.data$method)
+  
+  ################################################################################################
+  ### Processing prior to integration
+  ################################################################################################
+  print("Processing prior to integration")
+  # splits combined seurat object into each individual seurat
+  samples.combined = SplitObject(samples.combined, split.by="orig.ident")
+  
+  # normalises and finds variable features of individual seurats
+  samples.combined <- lapply(X = samples.combined, FUN = function(x) {
+    x <- NormalizeData(x)
+    if (config$recluster) {
+      x <- reCluster(x, files$GeneSignatures, config$alpha)
+    }
+    x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)
+    return(x)
+  })
+  
+  saveRDS(samples.combined, paste(files$output, "Rda/decontaminated_samples.Rda", sep="/"))
+  print("Completed decontamination")
+  return(samples.combined)
 }
 
-integrate_samples <- function (config) {
-	
+
+
+# Integrating samples
+integrate_samples <- function (config, files, samples.combined) {
+  print("Finding integration anchors")
+  samples.anchors <- FindIntegrationAnchors(object.list = samples.combined)
+ 
+  print("Integrating")
+  samples.combined <- IntegrateData(anchorset = samples.anchors)
+  
+  # Saving new cts
+  if (config$recluster)
+    write.table(as.matrix(Idents(samples.combined)), paste(files$output, "/new_clus.tsv", sep=""), sep="\t")
+  
+  DefaultAssay(samples.combined) <- "integrated"
+  
+  #### Dimension Reduction
+  print("Dimension reduction")
+  
+  samples.combined <- ScaleData(samples.combined, verbose = FALSE)
+  samples.combined <- RunPCA(samples.combined, npcs = 30, verbose = FALSE)
+  samples.combined <- RunUMAP(samples.combined, reduction = "pca", dims=1:30)
+  
+  samples.combined <- adding_metadata(samples.combined)
+  
+  saveRDS(samples.combined,paste(files$output, "Rda/integrated_rd.Rda", sep="/"))
+
+  return(samples.combined)
 }
 
 analyse_samples <- function (config) {
-
+  analyse_DEGs()
+  analyse_UMAPs()
+  if (config$recluster)
+    analyse_recluster()
 }
 
 ################
@@ -58,10 +154,39 @@ if (config$threads != 1) {
 }
 
 for (current_method in config$methods) {
+  print(paste(rep("#",30),collapse=""))
+  print(paste("Starting",current_method))
+  print(paste(rep("#",30),collapse=""))
+  
   files = get_files(config, current_method)
+  samples.combined=NULL
   if ("decontaminate" %in% config$process) {
-    decontaminate(config, files, current_method)
-  } 
+    print("Decontaminating")
+    samples.combined = decontaminate_samples(config, files, current_method)
+    print("Decontamination completed")
+  }
+  
+  if ("integrate" %in% config$process) {
+    if (is.null(samples.combined)) {
+      print("Reading Rda from file...")
+      samples.combined <- readRDS(paste(files$output, "Rda/decontaminated_samples.Rda", sep="/"))
+    }
+    
+    print("Integrating")
+    samples.combined = integrate_samples(config, files, samples.combined)
+    print("Integration completed")
+  }
+  
+  if ("analyse" %in% config$process) {
+    if (is.null(samples.combined)) {
+      print("Reading Rda from file...")
+      samples.combined <- readRDS(paste(files$output, "Rda/integrated_rd.Rda", sep="/"))
+    }
+    
+    print("Analysing")
+    samples.combined = analyse_samples(config, files, samples.combined)
+    print("Analysis completed")
+  }
   
 }
 # for each method...
--- scripts/general_functions.R
+++ scripts/general_functions.R
@@ -29,15 +29,16 @@ load_libraries <- function () {
 
 get_config <- function(args, testing=F) {
   if (!testing)
-    config <- config::get(config = (if (length(args) > 0) args[[1]]))
+    config <- config::get(config = (if (length(args) > 0) args[[1]]), file = "config.yml")
   else
     config <- config::get(config="harry")
   
   # TODO check for empty strings in critical places
   
-  # TODO split rest of "arrays"
-  config$sample_ids = strsplit(config$sample_ids, split = "|",fixed=T)[[1]]
-  
+  # splitting "arrays"
+  for (i in c("sample_ids", "genes_>=9_cts", "ct_order_dotplots", "pie_plot_cts")) {
+    config[[i]] = strsplit(config[[i]], split="|",fixed=T)[[1]]
+  }
   return(config)
 }
 
@@ -47,23 +48,25 @@ log_print <- function (msg) {
     print(msg)
 }
 
-
-
 get_files <- function (config, current_method) {
   files = list(
     "CellRanger" = paste(config$input_dir, "CellRanger", sep="/"),
-    "Filtered" = paste(config$input_dir, "Filtered_Barcode_Matrices", sep="/"),
+    "Filtered" = paste(config$input_dir, "Filtered_Feature_Barcode_Matrices", sep="/"),
     "CellBender" = paste(config$input_dir, "CellBender", sep="/"),
-    "CellAnnotations" = if (current_method == "none") paste(config$input_dir, config$original_cell_annotations_file, sep="/") 
-                        else paste(config$input_dir, config$new_cell_annotations_file, sep="/"),
-    "GeneSignatures" = paste(config$input_dir, "gene_signatures.xlsx", sep="/"),
-    "output" = paste(config$output_dir, current_method, sep="/")
+    "CellAnnotations" = if (config$recluster == F || (current_method == "none" || current_method == "no_decontamination")) 
+                          paste(config$input_dir, config$original_cell_annotations, sep="/") 
+                        else paste(config$input_dir, config$new_cell_annotations, sep="/"),
+    "GeneSignatures" = paste(config$input_dir, config$gene_signatures, sep="/"),
+    "output" = paste(config$output_dir, current_method, sep="/"),
+    "OcraRelDir" = paste(config$ocra_dir, current_method, sep="/")
   )
   
   if (substring(current_method,0,5) == "soupx") {
     files$dir = sapply(config$sample_ids, FUN = function(x) {
       paste(files$CellRanger, x, sep="/")
     }, USE.NAMES=F)
+    
+    files$special = rep(files$GeneSignatures, length(config$sample_ids))
   } else if (substring(current_method,0,7) == "decontx") {
     files$dir = sapply(config$sample_ids, function(x) {
       paste(files$Filtered,"/", x,".txt", sep="")
@@ -89,6 +92,84 @@ get_files <- function (config, current_method) {
   return(files)
 }
 
-files <- get_files(config, "soupx")
+get_clusters <- function(path, sample_id, use_new=FALSE) {
+  if (!use_new) {
+    is_preserved = length(str_split(sample_id, "_",simplify=TRUE))>2
+    
+    # Annotations for preserved cells are on a separate sheet within the file
+    if (!is_preserved) {
+      suppressMessages({
+        cell_annotations = read_excel(path, sheet=1)
+      })
+      # library names are different from preserved library names
+      cell_annotations = cell_annotations[cell_annotations$Library==sample_id,]
+      
+    } else {
+      suppressMessages({
+        cell_annotations = read_excel(path, sheet=2)
+      })
+      cell_annotations = cell_annotations[cell_annotations$Preservation == "MeOH",c(1:3,5)]
+      
+      cell_annotations = cell_annotations[cell_annotations$Library==str_split(sample_id, "_",simplify=TRUE)[1],]
+    }
+    
+    names(cell_annotations)[1] = "barcodes"
+    names(cell_annotations)[4] = "orig.ident"
+    
+    cell_annotations$barcodes = sapply(cell_annotations$barcodes, function(x) paste(substring(x, nchar(sample_id)+2), "-1", sep=""))
+    names(cell_annotations$orig.ident) = cell_annotations$barcodes
+    
+    #cell_annotations = subset(cell_annotations, select=-c(1,2,3))
+    return(cell_annotations$orig.ident)
+  } else {
+    annotations = read.table(path, sep="\t",header=F,skip=1,stringsAsFactors=F, as.is=T)
+    
+    names(annotations) = c("barcode", "celltype")
+    
+    # creating sample_id variable
+    annotations$sample_id = sapply(annotations$barcode, FUN=function(x) {
+      return(paste(head(str_split(x,"_",)[[1]],-1),collapse = "_"))
+    })
+    
+    annotations = annotations[which(annotations$sample_id==sample_id),]
+
+    
+    # removing sample_id from barcode
+    annotations$barcode = sapply(annotations$barcode, FUN=function(x) {
+      return(tail(str_split(x, "_")[[1]],1))
+    })
+    
+    ct = annotations$celltype
+    names(ct) = annotations$barcode
+    return(ct)
+  }
+}
 
 
+################################################################################################
+### Adds some metadata to the combined seurat object
+################################################################################################
+adding_metadata <- function(samples.combined) {
+  #order_paper <- rev(c("Fib", "MPH", "Podo", "aLOH", "Unknown","CD_PC", "T","NK","DCT","CNT","B","MC","CD_IC","Endo","PT","CD_Trans"))
+  Idents(samples.combined)[which(Idents(samples.combined)=="CD_Trans")] = "Unknown"
+  order_paper = config$ct_order_dotplots[which(config$ct_order_dotplots %in% levels(samples.combined))]
+  
+  #samples.combined <- readRDS(paste(output, "samples_integrated_rd.Rda", sep="/"))
+  DefaultAssay(samples.combined) <- "RNA"
+  
+  # adding metadata for `celltype_method`, `celltype`, and changing default ident to `celltype_method`
+  samples.combined$celltype_method <- paste(Idents(samples.combined), samples.combined$preservation, sep = "_")
+  samples.combined$celltype <- Idents(samples.combined)
+  Idents(samples.combined) <- "celltype_method"
+  
+  samples.combined$celltype.fresh = unlist(lapply(samples.combined$celltype_method, function(x) {
+    end <- (tail(c(str_split(x, fixed("_"),simplify=T)),n=1)=="fresh")
+    x <- if (end) str_sub(x,end=-7) else x
+  }))
+  samples.combined$celltype.meoh = unlist(lapply(samples.combined$celltype_method, function(x) {
+    end <- (tail(c(str_split(x, fixed("_"),simplify=T)),n=1)=="MeOH")
+    x <- if (end) str_sub(x,end=-6) else x
+  }))
+  
+  return(samples.combined)
+}
