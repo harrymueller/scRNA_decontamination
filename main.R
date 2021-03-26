88603737b2712d246d2cfe3e7652f6b936cfa26a
harrymueller <harry@mueller.com.au>
2021-03-26 06:08:33 +0000

    .

--- main.R
+++ main.R
@@ -88,7 +88,6 @@ decontaminate_samples <- function (current_method) {
     samples.combined@meta.data$preservation = factor(samples.combined@meta.data$preservation)
   samples.combined@meta.data$method = factor(samples.combined@meta.data$method)
   
-  saveRDS(samples.combined, paste(files$output, "Rda/temp.Rda", sep="/"))
   ### Processing prior to integration
   print("Processing prior to integration")
   # splits combined seurat object into each individual seurat
