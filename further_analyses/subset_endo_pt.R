37d4b79351bca5a52930e775807271c77c61a5cb
harrymueller <harry@mueller.com.au>
2021-09-29 06:58:10 +0000

    fixed subset endo pt

--- further_analyses/subset_endo_pt.R
+++ further_analyses/subset_endo_pt.R
@@ -2,27 +2,30 @@
 library(Seurat)
 library(Matrix)
 library(varhandle)
+library(stringr)
 
 source("scripts/clustering.R")
 
 # cannabilised from /other/merge_hgmm.R
-input_dir = "/data/Perkins/Mouse_Kidney/data_endo_pt"
-cell_annotations = "/data/Perkins/Mouse_Kidney/reclus/no_decontamination/new_clus.tsv"
+input_dir = "/data/decont_project/mouse_kidney/input_pt_endo"
+cell_annotations = "/data/decont_project/mouse_kidney/input_pt_endo/new_clus.tsv"
 sample_ids = c("BG1_BG20C", "BG3_BG21C", "BG5_BG22C", "BG52_BG20C_MeOH", "BG54_BG21C_MeOH", "BG56_BG22C_MeOH")
 
 cellranger <- function(sample_id, type) {
     # read in data
     dir = paste(input_dir, "CellRanger", sample_id, paste(type, "gene_bc_matrices", sep="_"), "mm10", sep="/")
-    print(dir)
     sample = Read10X(dir)
     sample = CreateSeuratObject(sample)
 
     # add clusters
-    cell_annotations = get_clusters(cell_annotations, sample_id, False)
-    Idents(sample) <- cell_annotations
+    cell_annotations = get_clusters(cell_annotations, sample_id, FALSE)
+    
     print(sample)
-    # subset
-    sample = sample[, Idents(sample) == "PT" || Idents(sample) == "Endo"]
+    
+    # barcodes to remove from the sample
+    to_delete = names(cell_annotations[cell_annotations != "PT" & cell_annotations != "Endo"])
+    sample = sample[, !colnames(sample) %in% to_delete]
+    
     print(sample)
     # output
     writeMM(sample@assays$RNA@counts, paste(dir, "matrix.mtx", sep="/"))
